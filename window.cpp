a66ac9ef24d0a67dc253726b5c7f98138142ab0b
nKadykov <nkadykov@gmail.com>
2024-04-01 11:45:04 +0300

    init

--- window.cpp
+++ window.cpp
@@ -1,6 +1,7 @@
 #include "window.h"
 #include <QSpinBox>
 #include <QGroupBox>
+#include <QLabel>
 #include <QHBoxLayout>
 #include <QLabel>
 
@@ -17,8 +18,14 @@ Window::Window(QWidget *parent)
 
 void Window::createSpinBoxes() {
     spinBoxesGroup = new QGroupBox(tr("Spinboxes"));
+
+    QLabel *integerLabel = new QLabel(tr("Enter a value between -20 and 20"));
     QSpinBox *integerSpinBox = new QSpinBox;
+    integerSpinBox->setRange(-20, 20);
 
     QVBoxLayout *spinBoxLayout = new QVBoxLayout;
+    spinBoxLayout->addWidget(integerLabel);
     spinBoxLayout->addWidget(integerSpinBox);
+
+    spinBoxesGroup->setLayout(spinBoxLayout);
 }
